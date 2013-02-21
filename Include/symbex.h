bda56ce02aabed773d31dc37b91640cc50d9c5f3
Stefan Bucur <stefan.bucur@epfl.ch>
2013-02-21 11:53:57 -0500

    Disabling interned & short-circuited optimizations.

--- Include/symbex.h
+++ Include/symbex.h
@@ -3,8 +3,8 @@
 #define _SYMBEX_ALLOC             1
 #define _SYMBEX_VARSIZE           1
 #define _SYMBEX_HASHES            1
-#define _SYMBEX_INTERNED          1
-#define _SYMBEX_SHORT_CIRCUITED   1
+/*#define _SYMBEX_INTERNED          1*/
+/*#define _SYMBEX_SHORT_CIRCUITED   1*/
 #define _SYMBEX_INSTRUMENT        1
 #endif
 
